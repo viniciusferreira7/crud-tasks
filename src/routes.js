991d60c6fd4fd322b5bdc0a188c574b93f4b58e4
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-08 22:12:40 -0300

    feat: creates the POST route and task insertion through it, and adds to GET the receipt of tasks

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,43 @@
+import { randomUUID } from 'node:crypto'
+
+import { Database } from './database.js'
+
+const database = new Database()
 export const routes = [
   {
     method: 'GET',
     path:'/tasks',
     handler:(req, res) => {
-     return res.writeHead(200).end('teste')
+
+    const data = database.select('tasks')
+    
+     return res.writeHead(200).end(JSON.stringify(data))
+    }
+  },
+
+  {
+    method: 'POST',
+    path:'/tasks',
+    handler:(req, res) => {
+
+      const { title, description } = req.body
+
+      const optionsDate = {
+        year:'numeric',
+        month:'2-digit',
+        day:'2-digit'
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: new Date().toLocaleString('pt-BR', optionsDate),
+      }
+
+      database.insert('tasks',task)
+
+      return res.writeHead(201).end()
     }
   }
 ]
